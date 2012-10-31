d2ffe1e8348583ba629e266361b912f65651bc6f
lex <ag@adhands.ru>
2012-10-31 13:34:25 +0400

    Improved test runner to write tests for grammar

--- pie/test/launcher.py
+++ pie/test/launcher.py
@@ -1,15 +1,18 @@
-import unittest
-import os
-import sys
-import tempfile
 from parser import Parser
 from pie.error import PHPError, InterpreterError
 from pie.interpreter.context import Context
 from pie.interpreter.frame import Frame
 from pie.parsing.parsing import interpret_file, InterpretedFile
+from pprint import pprint
+from pypy.rlib.parsing.deterministic import LexerError
 from pypy.rlib.parsing.parsing import ParseError
+import os
+import sys
+import tempfile
+import unittest
 
 __author__ = 'sery0ga'
+
 
 class TestPHPLanguageCoverage(unittest.TestCase):
 
@@ -21,13 +24,16 @@ class TestPHPLanguageCoverage(unittest.TestCase):
     def setUp(self):
         self.current_position = self.output_file.tell()
 
+
 def fill_test_class_with_tests(test_to_run = [], with_php_source = False):
     """ Read directory contains test files and create a test method for each
     test file
     """
-    _fill_test_class_with_tests_from_dir('/coverage/', test_to_run)
+    _fill_test_class_with_tests_from_dir('/coverage/', test_to_run, 'coverage_')
+    _fill_test_class_with_tests_from_dir('/grammar/', test_to_run, 'grammar_')
     if with_php_source:
         _fill_test_class_with_tests_from_dir('/from_php_source/', test_to_run)
+
 
 def _fill_test_class_with_tests_from_dir(directory, test_to_run, prefix = ''):
     """ Read directory contains test files and create a test method for each
@@ -40,10 +46,13 @@ def _fill_test_class_with_tests_from_dir(directory, test_to_run, prefix = ''):
         #  in their name
         entry_structure = entry.split('.')
         if len(entry_structure) == 1:
+            if not os.path.isdir(path + entry):
+                continue
+
             sub_dir = directory + entry + '/'
             _fill_test_class_with_tests_from_dir(sub_dir,
                                                  test_to_run,
-                                                 entry + '_')
+                                                 prefix + entry + '_')
             continue
         filename = entry_structure[0]
         test_name = 'test_' + prefix + filename
@@ -52,35 +61,53 @@ def _fill_test_class_with_tests_from_dir(directory, test_to_run, prefix = ''):
         full_filename = path + entry
         _add_test(full_filename, test_name)
 
+
 def _add_test(filename, test_name):
+
     def test(self):
         test = self.parser.parse(filename)
+
+        if test.skip:
+            if test.skip_reason:
+                self.skipTest(test.skip_reason)
+            else:
+                self.skipTest("Mark as skipped")
+            return
+
         # initialization
         self.context.initialize_function_trace_stack(filename)
-        file = InterpretedFile(filename, test.data)
+        interpreted_file = InterpretedFile(filename, test.data)
 
-        # create temporary file and redirect output to it
+        # create temporary interpreted_file and redirect output to it
         old_fileno = os.dup(sys.stdout.fileno())
         os.dup2(self.output_file.fileno(), 1)
-        try:
-            interpret_file(file, self.context, self.frame)
-            os.dup2(old_fileno, 1)
-        except PHPError as e:
-            os.dup2(old_fileno, 1)
-            print e.print_message()
-        except InterpreterError as e:
-            os.dup2(old_fileno, 1)
-            print e
-        except ParseError as e:
-            os.dup2(old_fileno, 1)
-            print e.nice_error_message(file.filename, file.data)
-        # rewind file pointer and read content
-        self.output_file.seek(self.current_position)
 
-        actual_result = self.output_file.read()
-        if test.is_true:
-            self.assertEqual(actual_result, test.result)
-        else:
-            self.assertNotEqual(actual_result, test.result)
+#        try:
+        interpret_file(interpreted_file, self.context, self.frame)
+        os.dup2(old_fileno, 1)
+#        except PHPError as e:
+#            os.dup2(old_fileno, 1)
+#            print e.print_message()
+#        except InterpreterError as e:
+#            os.dup2(old_fileno, 1)
+#            print e
+#        except LexerError as e:
+#            print e.nice_error_message(file.filename)
+#            sys.exit(1)
+#        except ParseError as e:
+#            os.dup2(sys.stdout.fileno(), 1)
+#            pprint(e.nice_error_message(interpreted_file.filename,
+#                                        interpreted_file.data))
+
+        if test.has_result:
+            # rewind file pointer and read content
+            self.output_file.seek(self.current_position)
+
+            actual_result = self.output_file.read()
+            if test.is_true:
+                self.assertEqual(actual_result, test.result)
+            else:
+                self.assertNotEqual(actual_result, test.result)
+
     test.__name__ = test_name
     setattr(TestPHPLanguageCoverage, test_name, test)
--- pie/test/parser.py
+++ pie/test/parser.py
@@ -5,66 +5,83 @@ __author__ = 'sery0ga'
 class Test(object):
 
     def __init__(self):
-        self.result = ''
-        self.data = ''
         self.doc = ''
+        self.data = ''
+        self.has_result = False
+        self.result = ''
         self.is_true = True
+        self.skip = False
+        self.skip_reason = ''
 
     def __str__(self):
-        return "Result: %s\n File: %s\n Doc: %s\n" \
-            % (self.result, self.data, self.doc)
+        return "Doc:\n%s\nFILE:\n%s\nRESULT:\n%s" \
+            % (self.doc, self.data, self.result)
 
 class Parser(object):
 
-    (initial, doc, file, result, skip) = range(5)
+    (INITIAL, DOC, FILE, RESULT, SKIP, SKIPIF) = range(6)
 
     def parse(self, filename):
         test = Test()
-        mode = self.initial
-        file = open(filename, 'r')
-        for line in file:
+        mode = self.INITIAL
+        file_handler = open(filename, 'r')
+        for line in file_handler:
             line = line.strip('\n')
             current_mode = mode
+
             if line == "--TEST--":
-                mode = self.doc
+                mode = self.DOC
                 continue
             elif line == "--FILE--":
-                mode = self.file
+                mode = self.FILE
+                continue
+            elif line == "--SKIP--":
+                mode = self.SKIP
+                test.skip = True
                 continue
             elif line == "--SKIPIF--":
-                mode = self.skip
+                # TODO
+                mode = self.SKIPIF
                 continue
             elif line == "--EXPECTF--" or line == "--EXPECT--":
-                mode = self.result
+                mode = self.RESULT
+                test.has_result = True
                 if line == "--EXPECTF--":
                     test.is_true = False
                 continue
-            if current_mode == self.doc:
+
+            if current_mode == self.DOC:
                 if not test.doc:
                     test.doc = line
                 else:
                     test.doc = "\n".join([test.doc, line])
                 continue
-            elif current_mode == self.result:
+            elif current_mode == self.RESULT:
                 if not test.result:
                     test.result = line
                 else:
                     test.result = "\n".join([test.result, line])
                 continue
-            elif current_mode == self.file:
+            elif current_mode == self.FILE:
                 if not test.data:
                     test.data = line
                 else:
                     test.data = "\n".join([test.data, line])
                 continue
-            elif current_mode == self.skip:
+            elif current_mode == self.SKIP:
+                if not test.skip_reason:
+                    test.skip_reason = line
+                else:
+                    test.skip_reason = "\n".join([test.data, line])
                 continue
-        file.close()
+
+        file_handler.close()
         return test
 
 
 if __name__ == "__main__":
     if len(argv) < 2:
-        print 'No input file provided'
+        print 'No input FILE provided'
         exit()
+
     print Parser().parse(argv[1])
